252b2107ef7e3fe8fc850f1a77c910ccca5c697b
Joshua Snyder <josh@cheetahfox.com>
2017-10-31 22:47:58 -0500

    Changed the Hup Ip address

--- hue-light-control.ino
+++ hue-light-control.ino
@@ -1,6 +1,6 @@
 // connection information for the Hue hub
-#define HUE_HUB_IP_ADDRESS_BYTES {192,168,77,204}
-#define HUE_HUB_IP_ADDRESS "192.168.77.204"
+#define HUE_HUB_IP_ADDRESS_BYTES {192,168,77,50}
+#define HUE_HUB_IP_ADDRESS "192.168.77.50"
 #define HUE_DEVELOPER_NAME "newdeveloper"
 #define HUE_LIGHT_CRM_1 "5"
 #define HUE_LIGHT_CRM_2 "6"
@@ -17,16 +17,16 @@ void setup() {
     pinMode(A0, INPUT);
     Particle.variable("computer_room", light);
     Particle.variable("A_read", read_val);
-    
+
     digitalWrite(D0,HIGH);
 }
 
 void loop() {
     bool state_crm = FALSE;
     if (analogRead(A0) > 80 ) {
-        state_crm = TRUE; 
+        state_crm = TRUE;
     }
-    
+
     if (state_crm != light) {
         light = state_crm;
         do_light_update(light,HUE_LIGHT_CRM_1, 254);
@@ -47,10 +47,10 @@ void do_light_update(bool state, String light_id, int bri) {
     if (client.connect(HUE_HUB_IP_ADDRESS_BYTES, 80)) {
         char* req;
         asprintf(&req, "PUT /api/%s/lights/%s/state HTTP/1.0", HUE_DEVELOPER_NAME, light_id.c_str());
-        
+
         char* host;
         asprintf(&host, "Host: %s", HUE_HUB_IP_ADDRESS);
-        
+
         char* on;
         if (state) {
             asprintf(&on, "true");
@@ -58,13 +58,13 @@ void do_light_update(bool state, String light_id, int bri) {
         else {
             asprintf(&on, "false");
         }
-        
+
         char* content;
         asprintf(&content, "{\"on\":%s,\"bri\":%i}",on, bri);
-        
+
         char* content_length;
         asprintf(&content_length, "Content-Length: %i", strlen(content));
-        
+
         client.println(req);
         client.println(host);
         client.println("Content-Type: application/json");
@@ -72,7 +72,7 @@ void do_light_update(bool state, String light_id, int bri) {
         client.println(content_length);
         client.println();
         client.println(content);
-        
+
         free(host);
         free(req);
         free(content_length);
@@ -83,4 +83,3 @@ void do_light_update(bool state, String light_id, int bri) {
         client.stop();
     }
 }
-
